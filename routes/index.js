42331253156626353429c8fa2dff2fda12dc2243
icheer <yao_ma@idgcapital.com>
2022-11-09 19:29:32 +0800

    rename

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ var conf = require('../config.js');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: '燃尽小纸条 - 相对安全' });
+  const ip = req.header('x-real-ip');
+  res.render('home', { title: '燃尽小纸条 - 相对安全' });
 });
 
 /**
@@ -14,6 +15,7 @@ router.get('/', function (req, res, next) {
  * @return {[type]}      [description]
  */
 router.get(conf.reg_guid, function (req, res) {
+  const ip = req.header('x-real-ip');
   res.render('read', { title: '查看纸条内容', reg_guid: conf.reg_guid });
 });
 
