0adc8fb335d3ac34570df09d084c08a1226cf5e4
icheer <yao_ma@idgcapital.com>
2023-01-13 13:52:31 +0800

    temp file

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ var conf = require('../config.js');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+  const ip = req.header('x-real-ip');
+  console.log('Creator IP: ' + ip);
   res.render('home', { title: '燃尽小纸条 - 相对安全' });
 });
 
@@ -14,6 +16,11 @@ router.get('/', function (req, res, next) {
  * @return {[type]}      [description]
  */
 router.get(conf.reg_guid, function (req, res) {
+  const ip = req.header('x-real-ip');
+  console.log('Reader IP: ' + ip);
+  if (true) {
+    console.log(req.header('user-agent'));
+  }
   res.render('read', { title: '查看纸条内容', reg_guid: conf.reg_guid });
 });
 
